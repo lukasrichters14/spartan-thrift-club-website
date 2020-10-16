a75055b0fa8b28a3f99bf2b5a1e626da6f6d151b
Lukas Richters <lukasrichters14@gmail.com>
2020-10-16 00:34:57 -0400

    Added firebase cloud function and updated member list.

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,9 +1,35 @@
 import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
+const secrets = require('./secrets');
+
+admin.initializeApp();
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'lukasrichters14@gmail.com',
+      pass: secrets.password
+    }
+  });
+
+exports.emailSender = functions.https.onRequest((req, res) =>
+{
+  const mailOptions =
+    {
+      from: 'lukasrichters14@gmail.com', // Adding sender's email
+      to: req.query.dest, // Getting recipient's email by query string
+      subject: 'Email Sent via Firebase', // Email subject
+      html: '<b>Sending emails with Firebase is easy!</b>' // Email content in HTML
+    };
+
+  return transporter.sendMail(mailOptions, (err: any, info: any) =>
+  {
+    if (err)
+    {
+      return res.send(err.toString());
+    }
+    return res.send('Email sent succesfully');
+  });
+});
